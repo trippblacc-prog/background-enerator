b912f0823695368bcbba9ae28ce95d2f37fe3306
trippblacc-prog <trippblacc@gmail.com>
2025-10-14 03:26:51 +0800

    adding starter project

---
+++ script.js
@@ -0,0 +1,15 @@
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.querySelector('#body')
+var h3 = document.querySelectorAll('h3')[0]
+
+
+function setGradient(){
+	body.style.background = 'linear-gradient( to right, ' + color1.value + ' , ' + color2.value + ')'
+	h3.textContent = body.style.background + ';';
+	
+}
+
+color1.addEventListener('input', setGradient)
+
+color2.addEventListener('input', setGradient)
